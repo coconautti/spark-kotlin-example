863b0e93f9356613c5442cb2b237c646d0773249
Peter Hägg <peter@coconautti.com>
2017-07-06 09:50:59 +0300

    Add JSON response formatter

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Basic template for web app using Spark with Kotlin.
 
 ## Todo
 
-- [ ] Add JSON response formatter
 - [ ] Add JOOQ
 - [ ] Add Guice
+- [x] Add JSON response formatter
 - [x] Add H2 JDBC connection
 - [x] Add Flyway
 - [x] Add console logger
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,47 +1,55 @@
+import com.fasterxml.jackson.databind.*
 import java.sql.*
 import org.flywaydb.core.*
 import spark.kotlin.*
 
 object Database {
-  val jdbcConnectionUrl = "jdbc:h2:file:./spark-kotlin"
+    val jdbcConnectionUrl = "jdbc:h2:file:./spark-kotlin"
 
-  fun migrate() {
-    val flyway = Flyway()
-    flyway.setDataSource(jdbcConnectionUrl, "", "")
-    flyway.clean() // <-- DANGER ZONE: DO NOT RUN IN PRODUCTION
-    flyway.migrate()
-  }
-
-  fun connection(): Connection = DriverManager.getConnection(jdbcConnectionUrl)
-
-  fun withConnection(body: (Connection) -> Unit) {
-    val conn = connection()
-    try {
-      body(conn)
-    } finally {
-      conn.close()
+    fun migrate() {
+        val flyway = Flyway()
+        flyway.setDataSource(jdbcConnectionUrl, "", "")
+        flyway.clean() // <-- DANGER ZONE: DO NOT RUN IN PRODUCTION
+        flyway.migrate()
     }
-  }
+
+    fun connection(): Connection = DriverManager.getConnection(jdbcConnectionUrl)
+
+    fun withConnection(body: (Connection) -> Any): Any {
+        val conn = connection()
+        try {
+            return body(conn)
+        } finally {
+            conn.close()
+        }
+    }
 }
+
+object Json {
+    val objectMapper = ObjectMapper()
+
+    fun toJson(model: Any?): String = objectMapper.writeValueAsString(model)
+}
+
+data class StatusReponse(val connectionStatus: String, val migrationStatus: Boolean)
 
 class Application
 
 fun main(args: Array<String>) {
-  Database.migrate()
+    Database.migrate()
 
-  val http: Http = ignite()
-  http.port(8080)
+    val http: Http = ignite()
+    http.port(8080)
 
-  http.get("/status") {
-    var response = ""
-    Database.withConnection { conn ->
-      val status = if (conn.isClosed) "closed" else "open"
-      response += "db test connection: ${status} \n"
-
-      val stmt = conn.createStatement()
-      val rs = stmt.executeQuery("SELECT * FROM users")
-      response += "db migrated: ${!rs.next()} \n"
+    http.after {
+        response.header("Content-Type", "application/json")
     }
-    response
-  }
+
+    http.get("/status") {
+        Database.withConnection { conn ->
+            val connectionStatus = if (conn.isClosed) "closed" else "open"
+            val migrationStatus = !conn.createStatement().executeQuery("SELECT * FROM users").next()
+            Json.toJson(StatusReponse(connectionStatus, migrationStatus))
+        }
+    }
 }
